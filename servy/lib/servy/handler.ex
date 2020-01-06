5bdd831412cac6b797e55b9c0ed869c3afeedae4
laura stevenson <laurastevenson@lauras-MacBook-Pro.local>
2020-01-06 10:42:54 -0800

    created request and response handler

--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -1,4 +1,34 @@
 defmodule Servey.Handler do
+  def handle(request) do
+    # create pipeline to call each function
+    # return value of previous function is
+    # passed in as first argument to following function
+    request |> parse |> route |> format_response
+  end
+
+  def parse(request) do
+
+    # parse request str and create map
+    conv = %{ method: "GET", path: "/wildthings", resp_body: ""}
+  end
+
+  def route(conv) do
+
+    # add value to resp_body key by creating a new map
+    conv = %{ method: "GET", path "/wildthings", resp_body: "Bears, Lions, Tigers"}
+  end
+
+  def format_response(conv) do
+
+    # use values on the map to create an HTTP reponse
+    """
+    HTTP/1.1 200 OK
+    Content-Type: text/html
+    Content-Length: 20
+
+    Bears, Lions, Tigers
+    """
+  end
 
 end
 request = """
@@ -15,3 +45,6 @@ Content-Length: 20
 
 Bears, Lions, Tigers
 """
+response = Servy.Handler.handle(request)
+
+IO.puts response
